0a87ed22857a7c27c23f8c3a48625b2a24b5b07b
Iceteatree <alankow11@gmail.com>
2022-12-03 11:15:11 +0200

    [ADDED] LN-001: Npm initialised, installed nodemon and adjusted app.js

--- 00-Node-Introduction/app.js
+++ 00-Node-Introduction/app.js
@@ -1,17 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 function rqListener(req, res) {
+    const url = req.url;
+    const method = req.method;
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
-    res.write(
-        `<html>
-            <body>
-                <h1>Hello World</h1>
-            </body>
-        </html>
-        `
-    );
-    res.end(); //Send back to the client. Don't write anything after it.
+    if (url === '/') {
+        res.write(
+            `<html>
+                <body>
+                    <h1>Hello World</h1>
+                </body>
+            </html>
+            `
+        );
+        return res.end();
+    }
+    if (url === '/test' && method === 'GET') {
+        fs.writeFileSync('test.txt', 'Dummy Test');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    return res.end(); //Send back to the client. Don't write anything after it.
 };
 
 const server = http.createServer(rqListener);
