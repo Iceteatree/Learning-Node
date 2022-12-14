594ecda6ff7c6643772b8495c09815603d3013fa
Iceteatree <alankow11@gmail.com>
2022-12-14 15:33:22 +0200

    [ADDED] LN-003: Assignment 2 completion and express practice

--- 03-Working-with-Express/app.js
+++ 03-Working-with-Express/app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
-const express = require('express');
+const express = require('express'); 
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.send('<h1>Add Product Page!</h1>'); // Send allows us to send a any response.
+    // If you're calling res, then you'll never want to call next();
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>'); // Send allows us to send a any response.
+});
 
-server.listen(3000);
+app.listen(3000);
