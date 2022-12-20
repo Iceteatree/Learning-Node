2e3492f282096dac9d27ceb6f13c59474220d55b
Iceteatree <alankow11@gmail.com>
2022-12-20 13:57:42 +0200

    [ADDED] LN-003: CSS has been added and served statically from public folder

--- 03-Working-with-Express/routes/admin.js
+++ 03-Working-with-Express/routes/admin.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../utils/path')
+
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
@@ -12,7 +14,7 @@ router.get('/add-product', (req, res, next) => {
     console.log('In another middleware!');
     // res.send('<h1>Add Product Page!</h1>'); // Send allows us to send a any response.
     // If you're calling res, then you'll never want to call next();
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
--- 03-Working-with-Express/routes/shop.js
+++ 03-Working-with-Express/routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path')
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     // console.log('In another middleware!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // Send allows us to send a any response.
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')); // Send allows us to send a any response.
 });
 
 module.exports = router;
