b79724eb1e6f0f9896221dfaf93ed7320c30f945
Iceteatree <alankow11@gmail.com>
2022-12-28 09:55:14 +0200

    [ADDED] LN-004: Handlebars templating engine

--- 04-Dynamic-content-and-templates/app.js
+++ 04-Dynamic-content-and-templates/app.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('handlebars', expressHbs());
+// app.set('view engine', 'pug');
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -19,7 +22,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404')
+    res.status(404).render('404', {pageTitle: '404 - Page not found'})
 });
 
 app.listen(3000);
