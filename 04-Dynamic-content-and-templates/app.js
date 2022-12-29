f399e835b803bf16b2d077eff3c01e4295169041
Iceteatree <alankow11@gmail.com>
2022-12-29 16:57:58 +0200

    [ADDED] LN-004: EJS templating engine work

--- 04-Dynamic-content-and-templates/app.js
+++ 04-Dynamic-content-and-templates/app.js
@@ -2,13 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
+// app.engine('handlebars', expressHbs({layoutDir: 'views/layouts/', defaultLayout: 'main-layout'}));
 // app.set('view engine', 'pug');
-app.set('view engine', 'handlebars');
+// app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
