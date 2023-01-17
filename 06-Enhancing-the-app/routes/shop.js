7d77ca2e8d3328371bd83313bbea3d744b6bd0ed
Iceteatree <alankow11@gmail.com>
2023-01-17 16:11:31 +0200

    [CHANGED] LN-006: Added new orders page and routed correctly

--- 06-Enhancing-the-app/routes/shop.js
+++ 06-Enhancing-the-app/routes/shop.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-const { getProducts, getIndex, getCart, getCheckout } = require('../controllers/shop');
+const { getOrders, getProducts, getIndex, getCart, getCheckout } = require('../controllers/shop');
 
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.get('/', getIndex);
 router.get('/products', getProducts);
 
 router.get('/cart', getCart);
+
+router.get('/orders', getOrders);
 
 router.get('/checkout', getCheckout);
 
