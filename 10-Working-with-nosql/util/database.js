ea7c16a83c73d828433bf2e8832bd48986e8ae2d
Iceteatree <alankow11@gmail.com>
2023-03-01 11:54:54 +0200

    [CHANGED] LN-010: Commented out all edit code and adjusted add code so that it functions correctly with mongoDB

--- 10-Working-with-nosql/util/database.js
+++ 10-Working-with-nosql/util/database.js
@@ -1,13 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (cb) => {
-    MongoClient.connect('mongodb+srv://alan-basic-user:alan123@cluster0-education.qfhv1.mongodb.net/?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://alan-basic-user:alan123@cluster0-education.qfhv1.mongodb.net/shop_learning_node?retryWrites=true&w=majority')
     .then(client => {
         console.log('Connected');
-        cb(client);
+        _db = client.db()
+        cb();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No Database Found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
