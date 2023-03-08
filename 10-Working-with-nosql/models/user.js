f5f0298d255e7bae89656fc8831bc33c91987e9a
Iceteatree <alankow11@gmail.com>
2023-03-08 10:56:52 +0200

    [ADDED] LN-010: New User Modal with updated controller functionality for products

--- 10-Working-with-nosql/models/user.js
+++ 10-Working-with-nosql/models/user.js
@@ -1,16 +1,34 @@
-const Sequalise = require('sequelize');
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
+    }
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequalise.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name: Sequalise.STRING,
-    email: Sequalise.STRING,
-});
+    save () {
+        const db = getDb();
+        return db.collection('users_learning_node').insertOne(this)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users_learning_node').find({_id: new mongodb.ObjectId(userId)}).next()
+        .then(user => {
+            console.log(user);
+            return user;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+}
 
 module.exports = User;
