b52a76d5bc7154e833a2192ba9e3353fd08bfef1
Iceteatree <alankow11@gmail.com>
2023-03-15 11:18:43 +0200

    [CHANGED] LN-010: Completed orders logic and fixed delete logic

--- 10-Working-with-nosql/models/product.js
+++ 10-Working-with-nosql/models/product.js
@@ -1,5 +1,6 @@
-const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 class Product {
   constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
@@ -14,54 +15,63 @@ class Product {
     const db = getDb();
     let dbOp;
     if (this._id) {
+      // Update the product
       dbOp = db
-      .collection('products_learning_node')
-      .updateOne({ _id: this._id }, { $set: this });
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
-      dbOp = db.collection('products_learning_node').insertOne(this)
+      dbOp = db.collection('products_learning_node').insertOne(this);
     }
     return dbOp
-    .then(result => {
-      console.log(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static fetchAll() {
     const db = getDb();
-    return db.collection('products_learning_node').find().toArray()
-    .then(products => {
-      console.log(products);
-      return products;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    return db
+      .collection('products_learning_node')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static findById(prodId) {
     const db = getDb();
-    return db.collection('products_learning_node').find({_id: new mongodb.ObjectId(prodId)}).next()
-    .then(product => {
-      console.log(product);
-      return product;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    return db
+      .collection('products_learning_node')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static deleteById(prodId) {
     const db = getDb();
-    return db.collection('products_learning_node').deleteOne({ _id: new mongodb.ObjectId(prodId) })
-    .then(result => {
-      console.log('Deleted');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    return db
+      .collection('products_learning_node')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
