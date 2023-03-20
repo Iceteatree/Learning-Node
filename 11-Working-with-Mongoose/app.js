7dffcf1e53b0687b54bee3915d24ee1a833caab5
Iceteatree <alankow11@gmail.com>
2023-03-20 11:39:49 +0200

    [CHANGED] LN-011: Mongoose added to Product model

--- 11-Working-with-Mongoose/app.js
+++ 11-Working-with-Mongoose/app.js
@@ -9,7 +9,7 @@ const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -22,14 +22,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('64084b650e0e6a4fa11e42a3')
-    .then(user => {
-        req.user =  new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+//     User.findById('64084b650e0e6a4fa11e42a3')
+//     .then(user => {
+//         req.user =  new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     })
+//     .catch(err => console.log(err));
+// })
 
 
 app.use('/admin', adminRoutes);
