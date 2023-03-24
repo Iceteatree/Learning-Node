a7ae0284c69b7a70dd4fbc29ed4fe85e70f0c1e5
Iceteatree <alankow11@gmail.com>
2023-03-24 10:43:46 +0200

    [CHANGED] LN-011: Updated User model to be relevant again

--- 11-Working-with-Mongoose/app.js
+++ 11-Working-with-Mongoose/app.js
@@ -9,7 +9,7 @@ const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,14 +22,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('64084b650e0e6a4fa11e42a3')
-//     .then(user => {
-//         req.user =  new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     })
-//     .catch(err => console.log(err));
-// })
+app.use((req, res, next) => {
+    User.findById('641d615ca6451a50b3ec9af1')
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
 
 
 app.use('/admin', adminRoutes);
@@ -40,6 +40,18 @@ app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPassword}@cluster0-education.qfhv1.mongodb.net/shop_learning_node?retryWrites=true&w=majority`)
     .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Alan',
+                    email: 'alan@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
         app.listen(3000);
     })
     .catch(err => {
