94835ca9531b6caa2389e0a7a49b10b53b7d155a
Iceteatree <alankow11@gmail.com>
2023-04-24 10:41:14 +0200

    [CHANGED] LN-012: Added conditional checks to the FE for authentication

--- 12-Sessions-and-Cookies/controllers/auth.js
+++ 12-Sessions-and-Cookies/controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split('=')[1].trim();
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -12,10 +11,17 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   User.findById('641d615ca6451a50b3ec9af1')
   .then(user => {
-      req.session,isLoggedIn = true;
+      req.session.isLoggedIn = true;
       req.session.user = user;
       res.redirect('/');
   })
   .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
 
