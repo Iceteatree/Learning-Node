5f5a0345383f98c673cbc59de6878597e8ff7578
Iceteatree <alankow11@gmail.com>
2023-05-23 10:53:26 +0200

    [ADDED] LN-013: CSRF package and implemented in app, controllers and views

--- 13-Adding-Authentication/app.js
+++ 13-Adding-Authentication/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
@@ -21,6 +22,8 @@ const store = new MongoDBStore({
     collection: 'sessions',
 });
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,6 +34,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -43,6 +47,12 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 })
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
