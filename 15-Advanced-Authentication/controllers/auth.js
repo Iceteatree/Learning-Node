e7a15a0d621f612679e9067e0bfa9c7b2dc34661
Iceteatree <alankow11@gmail.com>
2023-06-21 10:45:09 +0200

    [ADDED] LN-015: Added new post request for new password

--- 15-Advanced-Authentication/controllers/auth.js
+++ 15-Advanced-Authentication/controllers/auth.js
@@ -5,6 +5,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport');
 require('dotenv').config();
 
 const User = require('../models/user');
+const { reset } = require('nodemon');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
   auth: {
@@ -181,9 +182,38 @@ exports.getNewPassword = (req, res, next) => {
       pageTitle: 'New Password',
       errorMessage: message,
       userId: user._id.toString(),
+      passwordToken: token
     });
   })
   .catch(err => {
     console.log(err);
   });
 };  
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: {$gt: Date.now()},
+    _id: userId
+  })
+  .then(user => {
+    return bcrypt.hash(newPassword, 12)
+  })
+  .then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = undefined;
+    resetUser.resetTokenExpiration = undefined;
+    return resetUser.save();
+  })
+  .then(result => {
+    res.redirect('/login');
+  })
+  .catch(err => {
+    console.log(err);
+  })
+};
