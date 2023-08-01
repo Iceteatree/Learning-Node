1787f72d4aea609e79e2e15f3e4808a4efeff564
Iceteatree <alankow11@gmail.com>
2023-08-01 14:57:45 +0200

    [CHANGED] LN-016: Added more validation by changing controller funcitonality

--- 16-Understanding-Validation/controllers/auth.js
+++ 16-Understanding-Validation/controllers/auth.js
@@ -76,7 +76,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -86,13 +85,7 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg
     });
   }
-  User.findOne({email: email})
-  .then(userDoc => {
-    if (userDoc) {
-      req.flash('error', 'Email exists already, please pick a different one.');
-      return res.redirect('/signup');
-    }
-    return bcrypt.hash(password, 12)
+    bcrypt.hash(password, 12)
       .then(hashedPassword => {
       const user = new User({
         email: email,
@@ -113,10 +106,6 @@ exports.postSignup = (req, res, next) => {
       }).catch(err => {
         console.log(err);
       });
-  })
-  .catch(err => {
-    console.log(err);
-  });
 };
 
 exports.postLogout = (req, res, next) => {
