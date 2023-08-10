36be6c2feaf252a7e15ec255353af84c8d64b5e8
Iceteatree <alankow11@gmail.com>
2023-08-10 10:40:37 +0200

    [ADDED] LN-016: Added validation to login

--- 16-Understanding-Validation/controllers/auth.js
+++ 16-Understanding-Validation/controllers/auth.js
@@ -46,6 +46,17 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
+
   User.findOne({email: email})
     .then(user => {
       if (!user) {
